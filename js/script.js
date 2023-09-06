879cf6123f642476d37e8cb80769ea77556c261a
Xerckiem <mercadoxerckiem@gmail.com>
2023-09-06 11:49:39 -0400

    changes in index.html, style.css, and script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// const open_btn = document.querySelector('.fa-circle-info');
+// const close_btn = document.querySelector('.fa-circle-xmark');
+// const popup = document.querySelector('.popup');
+// const popup_content = document.querySelector('.popup-content');
+//
+// open_btn.addEventListener('click', () => {
+//     popup.style.display = 'flex';
+//     popup_content.style.cssText = 'animation: slide-in 0.5s ease; animation-fill-mode: forwards;';
+// });
+//
+// close_btn.addEventListener('click', () => {
+//     popup_content.style.cssText = 'animation: slide-out 0.5s ease; animation-fill-mode: forwards;';
+//     setTimeout(() => {
+//         popup.style.display = 'none';
+//     }, 500);
+// });
+
+'use strict';
+
 const open_btn = document.querySelector('.fa-circle-info');
 const close_btn = document.querySelector('.fa-circle-xmark');
 const popup = document.querySelector('.popup');
@@ -5,11 +24,13 @@ const popup_content = document.querySelector('.popup-content');
 
 open_btn.addEventListener('click', () => {
     popup.style.display = 'flex';
-    popup_content.style.cssText = 'animation: slide-in 0.5s ease; animation-fill-mode: forwards;';
+    popup_content.classList.add('slide-in');
+    popup_content.classList.remove('slide-out');
 });
 
 close_btn.addEventListener('click', () => {
-    popup_content.style.cssText = 'animation: slide-out 0.5s ease; animation-fill-mode: forwards;';
+    popup_content.classList.add('slide-out');
+    popup_content.classList.remove('slide-in');
     setTimeout(() => {
         popup.style.display = 'none';
     }, 500);
